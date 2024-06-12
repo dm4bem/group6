a40a034f64a20848cd0d39bbb6f102b6fc2591e3
carriercl <166366033+carriercl@users.noreply.github.com>
2024-06-12 09:13:42 +0200

    Update steady_state.py

--- steady_state.py
+++ steady_state.py
@@ -1,25 +1,19 @@
-#Bibliothèques:
 import numpy as np
-
-#Variables :
+#Variables of the model:
 nq, nθ = 24, 17 # number of flow-rates branches and of temperaure nodes
-ho, hi = 25, 8 # en W/m2/K
-lambda_wind = 1.015 #conductivité thermique de la fenêtre en W/m/K
-lambda_wall = 0.22 #conductivité thermique du mur; en W/m/K
-lambda_isol = 0.023 #conductivité thermique de l'isolant ; en W/m/K
-lambda_door = 0.17 #conductivité thermique de la porte ; en W/m/K
-H = 3 # hauteur sous plafond; en m 
-w_wall, w_isol, w_wind, w_door = 0.2, 0.1, 0.025, 0.05 #epaisseurs des murs, isolant, fenêtre et portes; en m
-Lwind, L_door, L1, L2, L3, L4, L5, L6 = 4, 1, 4, 3, 2.5, 3, 2, 6 #largeur des murs, fenêtre et portes; en m
-Swind, S_door, S1, S2, S3, S4, S5, S6 = H*Lwind, H*L_door, H*L1, H*L2, H*L3, H*L4, H*L5, H*L6  #surfaces des murs, fenêtre et portes; en m
-
-To = -5 # température extérieure; en °C 
-Ti = 18 # température intérieure; en °C
-E = 200 # en W/m2
-alphawall_o = 0.55 #coefficient d'absorbtion du mur pour du béton lisse
-alphawall_i = 1 #coefficient d'absorbtion du mur intérieur
-
-P_int=1000+200 #puissance produite dans la maison par deux personnes et un radiateur en W
+H = 3 # height; in m
+w_wall, w_insul, w_wind, w_door = 0.2, 0.1, 0.025, 0.05 # width of walls, insulation layers, windows and doors; in m
+L_wind, L_door, L1, L2, L3, L4, L5, L6 = 4, 1, 4, 3, 2.5, 3, 2, 6 # length of walls, windows and doors; in m
+S_wind, S_door, S1, S2, S3, S4, S5, S6 = H*Lwind, H*L_door, H*L1, H*L2, H*L3, H*L4, H*L5, H*L6  #surface of walls, windows and doors; in m^2
+To, Ti = -5, 18 # outside and inside temperatures; in °C
+E = 200 # solar irradiation; in W/m^2
+P_int=1000+200 # power produced in the house by two people and a radiator ; in W
+ho, hi = 25, 8 # outside and inside convection coefficients; in W/m2/K
+lambda_wind, lambda_wall, lambda_insul, lambda_door   = 1.015, 0.22, 0.023, 0.17 #thermal conductivities of the windows, the walls, the insulation layers and the doors; in W/m/K
+alphawall_o, alphawall_i = 0.55, 1 #outside and inside absorption coefficient for the walls
+rho_wall, rho_isol, rho_air = 2300, 16, 1.2 # densities of the walls, the insulation material and the air ; in kg/m^3
+c_wall, c_isol, c_air = 880, 1210, 1000 # specific heats of the walls, the insulation material and the air ; in J/(kg.K)
+ 
 
 #Matrix A
 A=np.zeros([nq, nθ])
@@ -65,26 +59,46 @@ G[18]=(ho+lambda_wind/(w_wind*2))*Swind
 G=np.diag(G)
 
 
-#Matrix b 
+#Matrix b
 b=np.zeros(nq)
 b[0]=b[3]=b[6]=b[7]=Ti
 b[23]=-Ti
 b[16]=b[18]=-To
+ 
 
-#Matrix f 
+#Matrix f
 f=np.zeros(nθ)
 f[11]=E*alphawall_o*S3
 f[8]=E*alphawall_i*S3
 f[16]=E*alphawall_o*S6
 f[13]=E*alphawall_i*S6
 f[7]=P_int
+ 
+
+#Matrix C
+C=np.zeros(nθ)
+C[0]=rho_wall*c_wall*w_wall*S2
+C[1]=rho_insul*c_insul*w_insul*S2
+C[2]=rho_wall*c_wall*w_wall*S1
+C[3]=rho_insul*c_insul*w_insul*S1
+C[4]=rho_wall*c_wall*w_wall*S5
+C[5]=rho_insul*c_insul*w_insul*S5
+C[6]=rho_air*c_air*L1*L2*H+rho_wall*c_air*w_wall*S4
+C[7]=rho_air*c_air*L6*L_wind*H
+C[9]=rho_insul*c_insul*w_insul*S3
+C[10]=rho_wall*c_wall*w_wall*S3
+C[12]=rho_air*c_air*w_wind*S_wind
+C[14]=rho_insul*c_insul*w_insul*S6
+C[15]=rho_wall*c_wall*w_wall*S6
+ 
+
+#Matrix y
+y=np.zeros(nθ)
+y[6]=y[7]=1
 
 
-# Temperature vector in steady-state
+# Steady-state
 θ = np.linalg.inv(A.T @ G @ A) @ (A.T @ G @ b + f)
-
-# Flow-rate vector in steady-state
 q = G @ (-A @ θ + b)
-
 
 print(f"The temperature in the room 1 is : θ6 = {θ[6]:.2f} °C, and the temperature in the room 2 is :θ7 = {θ[7]:.2f} °C")
